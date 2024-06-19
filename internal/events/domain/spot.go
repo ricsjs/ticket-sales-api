c3e14be72a0ecc3c3a9db6f371d207a15d5d1a7c
ricsjs <ricfilho0007@gmail.com>
2024-06-19 19:35:28 -0300

    feat: creation of event entities and spots and function of adding spots

---
+++ internal/events/domain/spot.go
@@ -0,0 +1,66 @@
+package domain
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrInvalidSpotNumber       = errors.New("invalid spot number")
+	ErrSpotNotFound            = errors.New("spot not found")
+	ErrSpotAlreadyReserved     = errors.New("spot already reserved")
+	ErrSpotNameTwoCharacters   = errors.New("spot name must be at least 2 characters long")
+	ErrSpotNameRequired        = errors.New("spot name is required")
+	ErrSpotNameStartWithLetter = errors.New("spot name must start with a letter")
+	ErrSpotNameEndWithNumber   = errors.New("spot name must end with a number")
+)
+
+type SpotStatus string
+
+const (
+	SpotStatusAvailable SpotStatus = "available"
+	SpotStatusSold      SpotStatus = "sold"
+)
+
+type Spot struct {
+	ID       string
+	EventID  string
+	Name     string
+	Status   SpotStatus
+	TicketID string
+}
+
+func NewSpot(event *Event, name string) (*Spot, error) {
+	spot := &Spot{
+		ID:      uuid.New().String(),
+		EventID: event.ID,
+		Name:    name,
+		Status:  SpotStatusAvailable,
+	}
+
+	if err := spot.Validate(); err != nil {
+		return nil, err
+	}
+	return spot, nil
+}
+
+func (s *Spot) Validate() error {
+	if s.Name == "" {
+		return ErrSpotNameRequired
+	}
+
+	if len(s.Name) < 2 {
+		return ErrSpotNameTwoCharacters
+	}
+
+	if s.Name[0] < 'A' || s.Name[0] > 'Z' {
+		return ErrSpotNameStartWithLetter
+	}
+
+	if s.Name[1] < '0' || s.Name[1] > '9' {
+		return ErrSpotNameEndWithNumber
+	}
+
+	return nil
+}
