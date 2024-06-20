2a10e8ef89d74021960a42f9173adf0709639e0f
ricsjs <ricfilho0007@gmail.com>
2024-06-20 15:01:18 -0300

    feat: calculate ticket price and validation

--- internal/events/domain/ticket.go
+++ internal/events/domain/ticket.go
@@ -1,5 +1,11 @@
 package domain
 
+import "errors"
+
+var (
+	ErrTicketPriceZero = errors.New("ticket price must be greater than zero")
+)
+
 type TicketType string
 
 const (
@@ -13,4 +19,22 @@ type Ticket struct {
 	Spot       *Spot
 	TicketType TicketType
 	Price      float64
+}
+
+func IsValidTicketType(ticketType TicketType) bool {
+	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
+}
+
+func (t *Ticket) CalculatePrice() {
+	if t.TicketType == TicketTypeHalf {
+		t.Price /= 2
+	}
+}
+
+func (t *Ticket) Validate() error {
+	if t.Price <= 0 {
+		return ErrTicketPriceZero
+	}
+
+	return nil
 }
